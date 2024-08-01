38a73e95963e42dd81d36ba77ec9b63880ad4306
Akanksha Kumari <youakanksha@gmail.com>
2024-08-02 00:24:43 +0530

    Practiced loop pattern

--- practice_problems_pw/q1_break.cpp
+++ practice_problems_pw/q1_break.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num5;
-    for(int i=1;i<=10;i++){
-        num5=i*5;
-        if(num5%7==0)
-        break;
-        else continue;
+    for (int i = 1; i <= 10; i++)
+    {
+        num5 = i * 5;
+        if (num5 % 7 == 0)
+            break;
     }
-    cout<<num5<<endl;
+    cout << num5 << endl;
     return 0;
 }
