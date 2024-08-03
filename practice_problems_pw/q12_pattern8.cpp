786c21b1bf368e1a64a1afbece41af6869faf567
Akanksha Kumari <youakanksha@gmail.com>
2024-08-04 01:09:59 +0530

    wip pattern problems

--- practice_problems_pw/q12_pattern8.cpp
+++ practice_problems_pw/q12_pattern8.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int m,n;
-    cin>>m;
-    cin>>n;
+int main()
+{
+    int m, n, j;
+    cin >> m;
+    cin >> n;
 
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            if(j==1)
-                cout<<"1";
-                else if (j==n)
-                cout<<n;
-                else 
-                cout<<" ";
+    for (int i = 1; i <= m; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i == 1 || i == m)
+                cout << j;
+            else if (j == 1)
+                cout << 1;
+            else if (j == n)
+                cout << n;
+            else
+                cout << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     return 0;
