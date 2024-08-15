919146fb8299dc8c5217d2c53bb066e0720cd041
Akanksha Kumari <youakanksha@gmail.com>
2024-08-15 15:24:39 +0530

    wip ch9 Pointers

---
+++ let_us_c/ch_9_pointers/eg1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=3;
+    cout<<"Address of i = "<<&i<<endl;
+    cout<<"Value of i = "<<i<<endl;
+    return 0;
+}
---
+++ let_us_c/ch_9_pointers/eg3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=3;
+    int *j;
+
+    j = &i;
+    
+    cout<<"Address of i="<<&i<<endl;
+    cout<<"Address of i="<<j<<endl;
+    cout<<"Adress of j="<<&j<<endl;
+    cout<<"Value of j="<<j<<endl;
+    cout<<"Value of i="<<i<<endl;
+    cout<<"Value of i="<<*(&i)<<endl;
+    cout<<"Value of i="<<*j<<endl;
+
+    return 0;
+}
